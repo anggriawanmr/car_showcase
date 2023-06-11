ccb52dedee75579bdf0657f6ab8df41ce14b8055
anggriawanmr <anggriawanmr@gmail.com>
2023-06-11 23:52:18 +0700

    Adding Navbar and Footer component :sparkles:

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import CustomButton from './CustomButton';
+import Image from 'next/image';
 
 const Hero = () => {
   const handleScroll = () => {};
@@ -27,7 +28,8 @@ const Hero = () => {
 
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="" />
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay"></div>
         </div>
       </div>
     </div>
