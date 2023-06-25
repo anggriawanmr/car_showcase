c0de00ef9a9297f1b1724020c5bc99cad6ac6703
anggriawanmr <anggriawanmr@gmail.com>
2023-06-25 08:37:57 +0700

    Editing SearchManufacturer component :hammer:

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,3 +1,9 @@
+'use client';
+
+import { useState } from 'react';
+import Image from 'next/image';
+import { Combobox, Transition } from '@headlessui/react';
+
 import { SearchManufacturerProps } from '@/types';
 import React from 'react';
 
@@ -5,7 +11,34 @@ const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
-  return <div>SearchManufacturer</div>;
+  const [query, setQuery] = useState('');
+
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car Logo"
+            />
+          </Combobox.Button>
+
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+
+          <Transition></Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
