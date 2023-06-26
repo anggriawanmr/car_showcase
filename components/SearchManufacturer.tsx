742a83e5bebe431e695937d43fbe5aa150bb9114
anggriawanmr <anggriawanmr@gmail.com>
2023-06-26 11:52:08 +0700

    Editing SearchManufacturer component :hammer:

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -26,7 +26,7 @@ const SearchManufacturer = ({
 
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           {/* Button for the combobox. Click on the icon to see the complete dropdown */}
           <Combobox.Button className="absolute top-[14px]">
@@ -56,28 +56,38 @@ const SearchManufacturer = ({
             afterLeave={() => setQuery('')} // Reset the search query after the transition completes
           >
             <Combobox.Options>
-              {filteredManufacturers.length === 0 && query !== '' ? (
+              {filteredManufacturers.map((item) => (
                 <Combobox.Option
-                  value={query}
-                  className="search-manufacturer__option"
-                >
-                  Create "{query}"
-                </Combobox.Option>
-              ) : (
-                filteredManufacturers.map((item) => (
-                  <Combobox.Option
-                    key={item}
-                    className={({ active }) => `
+                  key={item}
+                  className={({ active }) => `
                   relative search-manufacturer__option ${
                     active ? 'bg-primary-blue text-white' : 'text-gray-900'
                   }
                 `}
-                    value={item}
-                  >
-                    {item}
-                  </Combobox.Option>
-                ))
-              )}
+                  value={item}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {item}
+                      </span>
+
+                      {/* Show an active blue background color if the option is selected */}
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? 'text-white' : 'text-pribg-primary-purple'
+                          }`}
+                        ></span>
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))}
             </Combobox.Options>
           </Transition>
         </div>
