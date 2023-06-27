8833ff4d4aa8141323f7ac51944fd2a1cb93bf4d
anggriawanmr <anggriawanmr@gmail.com>
2023-06-27 11:59:45 +0700

    Adding calculateCarRent utils :sparkles:

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -11,7 +11,16 @@ interface CarCardProps {
 }
 
 const CarCard = ({ car }: CarCardProps) => {
-  return <div>CarCard</div>;
+  const { city_mpg, year, make, model, transmission, drive } = car;
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default CarCard;
