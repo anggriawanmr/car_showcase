eb4e2caac57b281165de9ac7ce14d72050b3067a
anggriawanmr <anggriawanmr@gmail.com>
2023-06-29 07:36:05 +0700

    Editing SearchBar component :hammer:

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,21 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
 
 import SearchManufacturer from './SearchManufacturer';
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('');
@@ -15,6 +28,7 @@ const SearchBar = () => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
     </form>
   );
