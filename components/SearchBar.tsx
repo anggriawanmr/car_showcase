dbfb75341565993d0e6ab58bfe570d192ed0d847
anggriawanmr <anggriawanmr@gmail.com>
2023-06-30 14:42:56 +0700

    Editing SearchBar component :hammer:

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -19,6 +19,7 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('');
+  const [model, setModel] = useState('');
 
   const handleSearch = () => {};
   return (
@@ -30,6 +31,26 @@ const SearchBar = () => {
         />
         <SearchButton otherClasses="sm:hidden" />
       </div>
+
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model"
+        />
+        <input
+          type="text"
+          name="model"
+          value="model"
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguain"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 };
